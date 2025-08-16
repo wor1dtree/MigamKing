bdfcd4cc781170146b883d32ed72eb73dae8fe4f
onzl-c <dmswisl1017@gmail.com>
2025-08-17 02:13:18 +0900

    eunji

--- Readable.py
+++ Readable.py
@@ -56,79 +56,39 @@ class Readable:
 		self.audio = pygame.mixer.Sound("Audio\\Misc\\talking.wav")
 
 	def update(self, king):
-
-		if king.idle_counter >= 200 and self.rect.colliderect(king.rect):
-
-			try:
-
-				if not self.blit_counter % self.interval:
-
-					next_letter = next(self.line)
-
-					if next_letter == "\n":
-
-						self.blit_counter = str(self.blit_counter)
-
-					else:
-
-						self.text += next_letter
-
-						if next_letter != " ":
-
-							self.channel.play(self.audio)
-
-				self.blit_counter += 1
-
-			except TypeError:
-
-				self.pause += 1
-
-				if self.pause > self.pause_interval / 4:
-
-					self.pause = 0
-					self.blit_counter = int(self.blit_counter)
-					self.text = ""
-
-			except StopIteration:
-
-				self.pause += 1
-
-				if self.pause > self.pause_interval:
-
-					self.reset()
-
-		else:
-
-			self.reset()
+		# if문과 else문을 모두 삭제하고 try 구문만 남깁니다.
+		try:
+			if not self.blit_counter % self.interval:
+				next_letter = next(self.line)
+				if next_letter == "\n":
+					self.blit_counter = str(self.blit_counter)
+				else:
+					self.text += next_letter
+					if next_letter != " ":
+						self.channel.play(self.audio)
+			self.blit_counter += 1
+		except TypeError:
+			self.pause += 1
+			if self.pause > self.pause_interval / 4:
+				self.pause = 0
+				self.blit_counter = int(self.blit_counter)
+				self.text = ""
+		except StopIteration:
+			self.pause += 1
+			if self.pause > self.pause_interval:
+				self.reset()
 
 	def reset(self):
-
 		self.line = iter(inspect.cleandoc(self.quote))
 		self.blit_counter = 0
 		self.pause = 0
 		self.text = ""
 
 	def blitmetext(self, screen):
-
 		if self.text:
-
 			for index, line in enumerate(map(lambda x: x[0], re.findall(r"(([^ .,!?]+[ .,!?]*){0,4})", self.text)[::-1])):
-
 				text = self.font.render(line, True, (255, 255, 255))
-
 				text_x, text_y = self.rect.x - text.get_width(), self.rect.y - (index + 1) * text.get_height()
-
 				if text_x < 0:
-
 					text_x = self.rect.right				
-
 				screen.blit(text, (text_x, text_y))
-
-if __name__ == "__main__":
-
-	text = 			"""I ha"""
-
-	#text2 = iter(text)
-	print(*map(lambda x: x[0], re.findall(r"((\w+[ .,!?]*){0,4})", text)))
-	
-	a = "" + 1
