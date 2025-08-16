79df075071d713e0e7e7ebd6f3c74036f55de6fc
onzl-c <dmswisl1017@gmail.com>
2025-08-17 02:01:24 +0900

    eunji

--- Readable.py
+++ Readable.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-#
-#
-#
-#
-
 import pygame
 import os
 import collections
@@ -13,70 +7,25 @@ import inspect
 class Readables:
 
 	def __init__(self):
-
 		self.font = pygame.font.Font("Fonts\\ttf_pixolde.ttf", 14)
-
-		self.readable_6 = ((304, 278, 29, 27), """I have had enough. My feet hurt from all the jumping, I have fallen too many times to count... How long has it been by now?
-
-												I should just stay here this time, yes, why try when victory is not certain?
-
-												All this struggle, for what? Never have I even been rewarded with as much as a glimpse!
-
-												I could get comfortable here... Yes, maybe I will try again some other time.
-
-												But then again, could it be the legends are true?""")
-
-		self.readable_10 =  ((175, 57, 23, 23), """Jumpers' Poem
-
-												Jumping and falling, my heart is turned cold
-
-												Down here naught can warm it, no not even gold
-
-												So I rise and attempt once again, fearing not
-
-												My heart resting sure that the babe remains hot""")
-
-		self.readable_15 = ((329, 239, 31, 33), """"Town-Hall Board of Notice"
-
-												This abandoned town, which is clearly abandoned, and which no-one lives in,
-
-												is henceforth irrevocably, eternially, foreverally,
-
-												uncancellably and undeniably claimed, by right of previous owners blatant abandonement,
-
-												as the sole property of Merchant Megildus Dreeg.
-
-												In addition I, Megildus Dreeg, shall rename this new shining centre of commerce, it shall hereby be known as Bargainburg!
-
-												Should any legal inquiries or complaints arise, contact *unintelligible*, thank you for your cooperation!""")
-
-		self.readable_21 = ((0, 308, 35, 35),   """Day 187
-
-												It appears my journey is at its end.
-
-												It seems it was not meant to be me after all.
-
-												For so long I believed...
-
-												But when I reached THAT place, there was nothing I could do...
-
-												Perhaps I can find someplace to stay back in Bogtown. 
-
-												I wonder if they will believe me...""")
-
+		self.readable_0 = ((304, 278, 29, 27), """Let's jump to window 10 !!
+					 							You can do it !""")
+		self.readable_3 =  ((175, 57, 23, 23), """
+												Let't jump to page table !
+					  							First, PML4 TABLE
+					  							Second, PDP TABLE
+					  							Third, DIR
+					  							Finally, PAGE TABLE !!!
+					  							Tang! Tang! Tang! Tang!""")
+		self.readable_7 = ((329, 239, 31, 33), """Lastly, Let's rescue the bread man
+												Jump to the bob center !!!""")
 		self.readables = collections.defaultdict()
-
 		self._load_readables() 
 
 	def _load_readables(self):
-
-		self.readables[6] = Readable(self.readable_6[0], self.readable_6[1], self.font)
-
-		self.readables[10] = Readable(self.readable_10[0], self.readable_10[1], self.font)
-
-		self.readables[15] = Readable(self.readable_15[0], self.readable_15[1], self.font)
-
-		self.readables[21] = Readable(self.readable_21[0], self.readable_21[1], self.font)
+		self.readables[0] = Readable(self.readable_0[0], self.readable_0[1], self.font)
+		self.readables[3] = Readable(self.readable_3[0], self.readable_3[1], self.font)
+		self.readables[7] = Readable(self.readable_7[0], self.readable_7[1], self.font)
 
 class Readable:
 
